286f82a991fe74e68b500e4abf48b218c8842e6f
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2020-09-24 11:39:35 -0300

    inserção de usuário com POST

--- src/main/java/com/rafaelbaetapena/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelbaetapena/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelbaetapena.workshopmongo.domain.User;
+import com.rafaelbaetapena.workshopmongo.dto.UserDTO;
 import com.rafaelbaetapena.workshopmongo.repository.UserRepository;
 import com.rafaelbaetapena.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrato"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
